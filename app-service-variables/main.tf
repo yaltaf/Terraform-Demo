ce36a97491b23648b3d4a318e7d15a29a86c2192
yaltaf <58113213+yaltaf@users.noreply.github.com>
2020-02-13 12:06:52 +0100

    Update main.tf

--- app-service-variables/main.tf
+++ app-service-variables/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "test" {
-  name     = var.resource_group_name
+  name     = "__resource_group_name__"
   location = var.location
 }
 
 resource "azurerm_app_service_plan" "test" {
-  name                =  var.appserviceplan
+  name                =  "__appserviceplan__"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                =  var.appservice
+  name                =  "__appservice__"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
