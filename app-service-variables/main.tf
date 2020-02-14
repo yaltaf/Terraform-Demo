4f9cf1bab2f9c88eef0461dbd1c9d1640855d5b8
yaltaf <58113213+yaltaf@users.noreply.github.com>
2020-02-14 17:16:39 +0100

    Update main.tf

--- app-service-variables/main.tf
+++ app-service-variables/main.tf
@@ -20,18 +20,4 @@ resource "azurerm_app_service" "test" {
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
 
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
 }
